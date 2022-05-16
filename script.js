28fc13fbbc3f1fe88042a98532355a423ea23eed
gustavo <penarandagustavo5@gmail.com>
2022-05-16 16:38:53 -0500

    config clear btn

--- script.js
+++ script.js
@@ -2,26 +2,21 @@ const display = document.getElementById('display');
 const clearBtn = document.getElementById('clear');
 const deleteBtn = document.getElementById('delete');
 const pointBtn = document.getElementById('point');
-
-/* const plusSign = document.getElementById('plus');
-const minusSign = document.getElementById('minus');
-const productSign = document.getElementById('product');
-const divisionSign = document.getElementById('division'); */
+const equalBtn = document.getElementById('equal');
 
 const digitsBtns = document.querySelectorAll('.digits');
 const operatorBtns = document.querySelectorAll('.operators');
 
 let displayNumber = '';
-let digitClicked = false;
-let operatorClicked = false;
+let countClick = 0;
+let num1 = '';
+let num2 = '';
 
 digitsBtns.forEach(digit => {
   digit.addEventListener('click', () => {
     if(displayNumber.length <= 14) {
     displayNumber += digit.innerText;
     display.innerText = displayNumber;
-    digitClicked = true;
-    operatorClicked = false;
     }
   });
 });
@@ -30,11 +25,20 @@ pointBtn.addEventListener('click', () => {
   if(!displayNumber.includes('.')) displayNumber += '.';
 });
 
-deleteBtn.addEventListener('click', () => {
+deleteBtn.addEventListener('click', deleteB);
+clearBtn.addEventListener('click', clearAll);
+
+function clearAll() {
+  display.innerText = '0';
+  displayNumber = '';
+  num1 = '';
+  num2 = '';
+}
+
+function deleteB() {
   if(displayNumber.length) {
     displayNumber = displayNumber.slice(0, -1);
     if(!displayNumber.length) display.innerText = '0';
     else display.innerText = displayNumber;
   }
-});
-
+}
